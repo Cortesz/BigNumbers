9aa7623b43f74f7275bc49d5b854b13fefdff269
Luccas <luccas.h.cortes@hotmail.com>
2023-11-22 22:46:59 -0300

    22/11

--- bignumber.c
+++ bignumber.c
@@ -13,7 +13,6 @@ BigNumber bignumber(){
     //Inicia o vetor com tamanho 1 e valor 0
     bn->data = malloc(sizeof(char));
     bn->data[0]=0;
-
     return bn;
 }
 
@@ -21,26 +20,72 @@ void le_bignumber(BigNumber bn){
     char* string;
     string = calloc(MAX,sizeof(char));
     fgets(string,MAX,stdin);
-    bn->size=strlen(string);
-    bn->sinal=1;
+
+    //Negativo
+    if(string[0]=='-'){
+        bn->size=strlen(string)-1;
+        bn->data = calloc(bn->size,sizeof(char));
+        for (int i = 0; i < bn->size; i++)
+        {
+        bn->data[bn->size-i-1]=string[i+1];
+        }
+        bn->sinal=0;
+    } 
     
-    for (int i = 0; i < bn->size; i++)
-    {
-        bn->data[i]=string[i];
+    //Positivo
+    else {
+        bn->size=strlen(string);
+        bn->data = calloc(bn->size,sizeof(char));
+      for (int i = 0; i < bn->size; i++)
+        {
+        bn->data[bn->size-i-1]=string[i];
+        }
+        bn->sinal=1;  
     }
     
+    //Libera a string auxiliar
     free(string);
 }
 
  void soma_bignumber(BigNumber a, BigNumber b){
 
+    BigNumber x;
+    x = bignumber();
+    x->size = fmax(a->sinal,b->size)+1;
+    x->data = calloc(x->size,sizeof(char));
+    x->sinal= a->sinal;
+
+    int aux=0;
+    char resultado;
+    for (int i = 0; i > x->size-1; i++)
+    {
+        if(i>a->size) resultado = b->data[i]+aux;
+        else if(i>b->size) resultado = a->data[i]+aux;
+        else resultado = a->data[i]+ b->data[i]+aux;
+
+        //Se a soma passar de 10, passar um para o proximo digito
+        if(x->data[i]>9){
+            resultado-=10;
+            aux=1;
+        } else {
+            aux=0;
+        }
+
+        x->data[i] = resultado;  
+    }
+    a = calloc(x->size,sizeof(char));
+    a->data = x->data;
+    a->size = x->size;
+    free(x);
  }
 
  void print_bignumber(BigNumber a){
+    printf("Bignumber = ");
     for (int i = 0; i < a->size; i++)
     {
-        printf("%c",a->data[i]);
-    }    
+        printf("%c",a->data[a->size-i]);
+    }
+    printf("\n");    
  }
 
  void free_bignumber(BigNumber a){
--- client.c
+++ client.c
@@ -4,10 +4,17 @@
 #include "bignumber.h"
 
 int main(){
-    BigNumber a;
+    BigNumber a,b;
     a = bignumber();
     le_bignumber(a);
+
+    b = bignumber();
+    le_bignumber(b);
+
+    printf("Soma \n"); 
+    soma_bignumber(a,b);
     print_bignumber(a);
+    
 
     free_bignumber(a);
     return 0;
